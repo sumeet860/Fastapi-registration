b0f55d62995db03b0f20f5d8bb530b68f4a16107
sumeet860 <41811254+sumeet860@users.noreply.github.com>
2020-11-30 00:07:15 +0530

    user registration with jwt generation

---
+++ schemas.py
@@ -0,0 +1,48 @@
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+
+class ItemBase(BaseModel):
+    title: str
+    description: Optional[str] = None
+
+
+class ItemCreate(ItemBase):
+    pass
+
+
+class Item(ItemBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    email: str
+
+
+class UserCreate(UserBase):
+    username: str
+    full_name: str
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    items: List[Item] = []
+
+    class Config:
+        orm_mode = True
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
